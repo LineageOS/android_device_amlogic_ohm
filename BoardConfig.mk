b7c0832b59ab809fb8bddd4413459c9636a06c73
Cosmin Tanislav <demonsingur@gmail.com>
2026-07-16 01:35:29 +0300

    ohm: build kernel with bazel
    Change-Id: Ifac009c172b76f8fcbf2a7a5f2604187197ec627

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,16 @@ TARGET_BOOTLOADER_BOARD_NAME := ohm
 
 ## DTB
 TARGET_DTB_NAME := sc2_s905x4_ah212_drm
+TARGET_DTBO_NAME := android_overlay_dt
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+## Kernel
+TARGET_KERNEL_PLATFORM_TARGET := ohm
+TARGET_KERNEL_SOURCE := vendor/amlogic/ohm-build
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/vendor_dlkm.modules.load))
+BOOT_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/vendor_boot.modules.load))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
 
 ## Partitions
 BOARD_SUPER_PARTITION_SIZE := 1887436800
