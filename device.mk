ee2f46ee889bd2a663a14df74b5b7429ed4b11f9
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-31 15:55:48 -0400

    [DNM] ohm: Disable Wi-Fi and BT
    * Until we wire the HAL and kernel module properly.
    
    Change-Id: If87e85359d80bedb134fcff274fb0d547090a570

--- device.mk
+++ device.mk
@@ -5,13 +5,11 @@
 #
 
 ## Bluetooth
-PRODUCT_PACKAGES += \
-    OhmBluetoothOverlay \
-    libbt-vendor
+BOARD_HAVE_BLUETOOTH := false
 
 ## Init-Files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
 
 ## Keylayout (IR)
 PRODUCT_COPY_FILES += \
@@ -23,6 +21,9 @@ TARGET_AMLOGIC_SOC := sc2
 ## Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+## Wi-Fi
+BOARD_HAVE_WIFI := false
 
 ## Inherit from the common tree product makefile
 $(call inherit-product, device/amlogic/ne-common/ne.mk)
