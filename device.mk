415af1684ba1a0a7aaa85d551ae85d660dd21cfc
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-31 15:25:52 -0400

    ohm: Rebrand from deadpool to ohm
    * Leave fingerprint/NRDP as deadpool to mirror devkits
      and maintain GMS certification status.
    * Set a proper OEM key value for ohm.
    * Update keylayout from ohm, but leave DPAD_CENTER as WAKE.
    * Use shipped target jarvis's 14 release for most blobs except:
      * DRM: libeomcrypto/libsecmem must be from source device.
      * TEE Applets: Must be from the source device for the most part.
      * Wi-Fi: Simply not present on jarvis's 14 release.
    * Swap from BCM to QCA Wi-Fi/BT.
      * TODO: Wire up HAL, use no-HAL path for now, it works fine.
    
    Change-Id: I0374c62658385f4f7e7ffbd9da16cd3ff149ba38

--- device.mk
+++ device.mk
@@ -6,14 +6,7 @@
 
 ## Bluetooth
 PRODUCT_PACKAGES += \
-    DeadpoolBluetoothOverlay \
-    libbt-vendor
-
-$(call soong_config_set,brcm_libbt,bdroid_buildcfg_include_dir,$(LOCAL_PATH)/bluetooth/include)
-$(call soong_config_set,brcm_libbt,custom_bt_config,//$(LOCAL_PATH):vnd_deadpool.txt)
-
-## Bluetooth firmware
-include kernel/amlogic/kernel-modules/dhd-driver/firmware/bluetooth/bluetooth.mk
+    OhmBluetoothOverlay
 
 ## Init-Files
 PRODUCT_COPY_FILES += \
@@ -23,16 +16,22 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl
 
+## Platform
+TARGET_AMLOGIC_SOC := sc2
+
 ## Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    hardware/broadcom/libbt
+    $(LOCAL_PATH)
 
-## Wi-Fi firmware
-include kernel/amlogic/kernel-modules/dhd-driver/firmware/wifi/wifi.mk
+## Wi-Fi
+TARGET_HAVE_WIFIHAL := false
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom-caf/wlan \
+    hardware/qcom-caf/wlan/qcwcn
 
 ## Inherit from the common tree product makefile
-$(call inherit-product, device/amlogic/g12-common/g12.mk)
+$(call inherit-product, device/amlogic/ne-common/ne.mk)
 
 ## Inherit from the proprietary files makefile
-$(call inherit-product, vendor/askey/deadpool/deadpool-vendor.mk)
+$(call inherit-product, vendor/amlogic/ohm/ohm-vendor.mk)
