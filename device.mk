5596a353bb2bb3a3fd6e80f63fca0a954dc4f82d
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-31 17:29:02 -0400

    ohm: S905X4 is an SC2 family SoC
    Change-Id: I67bb5b93ee573251cddf965ce194dbef879392ce

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl
 
+## Platform
+TARGET_AMLOGIC_SOC := sc2
+
 ## Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
