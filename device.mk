54edba614f47bcd93b51a3ec8b425a97c8f07610
Bruno Martins <bgcngm@gmail.com>
2026-08-29 12:41:50 +0100

    ohm: Rename and migrate init to blueprint
    Change-Id: I0013cac7636849924b0783775f22cfc379e470b3

--- device.mk
+++ device.mk
@@ -7,9 +7,9 @@
 PRODUCT_PACKAGES += \
     BluetoothOverlayTarget
 
-## Init-Files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
+## Init
+PRODUCT_PACKAGES += \
+    init.amlogic.wifi_buildin.rc
 
 ## Keylayout (IR)
 PRODUCT_COPY_FILES += \
